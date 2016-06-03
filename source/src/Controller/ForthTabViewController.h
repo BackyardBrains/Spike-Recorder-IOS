950f4d2df809916f18b49c3c5df11c77527a319f
stanislavmircic <stanislavmircic@gmail.com>
2016-06-03 15:11:24 -0400

    Grasshopper views enabled

---
+++ source/src/Controller/ForthTabViewController.h
@@ -0,0 +1,14 @@
+//
+//  ForthTabViewController.h
+//  Spike Recorder
+//
+//  Created by Stanislav Mircic on 6/3/16.
+//  Copyright © 2016 Datta Lab, Harvard University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ForthTabViewController : UIViewController
+- (IBAction)openExperiments:(id)sender;
+
+@end
---
+++ source/src/Controller/ForthTabViewController.m
@@ -0,0 +1,39 @@
+//
+//  ForthTabViewController.m
+//  Spike Recorder
+//
+//  Created by Stanislav Mircic on 6/3/16.
+//  Copyright © 2016 Datta Lab, Harvard University. All rights reserved.
+//
+
+#import "ForthTabViewController.h"
+
+@interface ForthTabViewController ()
+
+@end
+
+@implementation ForthTabViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)openExperiments:(id)sender {
+}
+@end
