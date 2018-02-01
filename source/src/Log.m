5f4ec3d6101ab95ca2c1bf464d98e086550adce1
stanislavmircic <stanislavmircic@gmail.com>
2018-02-01 12:40:04 +0100

    Before introducing segue for file view

--- source/src/Log.m
+++ source/src/Log.m
@@ -3,7 +3,7 @@
 //  Spike Recorder
 //
 //  Created by Stanislav Mircic on 7/23/15.
-//  Copyright (c) 2015 Datta Lab, Harvard University. All rights reserved.
+//  Copyright (c) 2015 BackyardBrains
 //
 
 #import "Log.h"
